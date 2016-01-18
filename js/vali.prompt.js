5fd79622227804c2117ee1a77ffe96b527237a12
鲁康 <1029131145@qq.com>
2016-01-18 16:22:07 +0800

    2016-01-18 16:22:02
    2016-01-18 16:22:02

---
+++ js/vali.prompt.js
@@ -0,0 +1,21 @@
+var vali_prompt = {
+    "Success": "Success",
+    "email": "Please enter the correct email address",
+    "phone": "Please enter the correct cell phone number",
+    "max": "Can not exceed ｛0｝ characters",
+    "min": "Not less than ｛0｝ characters",
+    "bank": "Please enter the correct bank card number",
+    "only": "Contain only",
+    "must": "Must contain",
+    "first_must": "The first must be：",
+    "first_cannot": "The first cannot be：",
+    "number": "Please enter a number",
+    "idcard": "Please enter the correct ID number",
+    "equally": "Must be the same",
+    "cannot": "cannot is ｛0｝",
+    "date": "Please enter the correct date",
+    "value0": "Chinese character",
+    "value1": "Letter",
+    "value2": "Number",
+    "value3": "Space character"
+};
