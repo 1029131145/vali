191b10433988e365eba82a7f20c34bcef9f01c94
鲁康 <1029131145@qq.com>
2016-01-20 15:24:11 +0800

    2016-01-20 15:24:08
    2016-01-20 15:24:08

--- js/canvas.js
+++ js/canvas.js
@@ -60,7 +60,6 @@ $(function(){
         this.radius = Math.random() * 2;
 
         this.color = new Color();
-        console.log(this);
     }
 
     Dot.prototype = {
@@ -205,7 +204,7 @@ $(function(){
         this.radius = Math.random() * 2;
 
         this.color = new Color();
-        console.log(this);
+
     }
 
     Dot.prototype = {
